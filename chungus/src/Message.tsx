a29988d3c0e9f3c144172d08dac52da544506139
Alex Hued <66076614+HuedCode88@users.noreply.github.com>
2025-11-08 13:51:29 -0500

    Im sorry.

--- chungus/src/Message.tsx
+++ chungus/src/Message.tsx
@@ -3,7 +3,7 @@ function Message() {
     //JSX: JavaScript XML
     const name = "Alex";
     if (name)
-        return <h1>Hello {name}</h1>;
+        return <h1>Hello {name} The Code is Working Don't Kill yourself</h1>;
     return <h1>Hello World</h1>;
 }
 
